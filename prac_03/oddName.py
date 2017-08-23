e8cd63fb4be7de8b8d234bc09e727d508e67ff60
JackSmithENG <jack.smith1@my.jcu.edu.au>
2017-08-23 13:01:59 +1000

    "Added odd_names file to prac_03" "Added temperatures file to prac_03" "Added broken_score file to prac_03"

--- prac_03/oddName.py
+++ prac_03/oddName.py
@@ -1,7 +1,26 @@
-""" Jack Smith """
+""" Jack Smith
 name = input("Enter name:")
 while name =="":
     print("Invalid name")
     name = input("Enter name:")
 
-print(name[::2])
+print(name[::2]) """
+
+def main():
+    name = get_name()
+    print_parts(name, 3)
+
+
+def print_parts(name, step=2):
+    print(name[::step])
+
+
+def get_name():
+    name = input("Name: ")
+    while name == "":
+        print("Invalid name.")
+        name = input("Name: ")
+    return name
+
+
+main()
